a032c7072da56f7e572759e72a5c028471ab36e1
nikhilrathod49 <rathod.nikhil49@gmail.com>
2024-03-10 13:12:49 +0530

    first commit

---
+++ src/main/java/com/spring/student/Student.java
@@ -0,0 +1,12 @@
+package com.spring.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	
+	@Autowired
+	StudentDetails studentDetails;
+ 
+}
